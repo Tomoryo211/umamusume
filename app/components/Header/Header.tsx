95ac6b81422a39e203d8cdc013460cc1e32ff942
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-15 12:18:34 +0900

    ロゴサイズ変更

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import styles from "./Header.module.scss";
 export default function Header () {
     return(
         <div className={styles.header}>
-            <Image src={Logo} alt="ウマ娘のロゴ" width={110} height={110} className={styles.Logo}/>
+            <Image src={Logo} alt="ウマ娘のロゴ" width={100} height={100} className={styles.Logo}/>
         </div>
     )
 }
