1cf31c3995112a8544f72d365470445decf189b2
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-22 23:05:29 +0900

    名前変更

--- app/components/Display/Display.tsx
+++ app/components/Display/Display.tsx
@@ -2,6 +2,9 @@ import Item from "../Display_item/Item";
 
 export default function Display_item(){
     return(
-        
+        <div>
+            <h1>名前</h1>
+            <h2>タイトル</h2>
+        </div>
     )
 }
