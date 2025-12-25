83f83bb9d92362f02f3e1aa15e4cb4f4c71f730a
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-25 21:00:04 +0900

    itemコンポーネント変更(説明文)

--- app/components/Display_item/item.tsx
+++ app/components/Display_item/item.tsx
@@ -4,7 +4,9 @@ export default function Item (){
     return(
         <>
         <div>
+            <h1>タイトル</h1>
             <p>画像</p>
+            <p>そのキャラのコメント</p>
         </div>
         </>
     )
