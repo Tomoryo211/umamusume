040b02d69a7883a325187e990520aa8063dce1f8
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-12-28 12:52:20 +0900

    scss変更

--- app/components/Display/Display.tsx
+++ app/components/Display/Display.tsx
@@ -3,10 +3,14 @@ import Styles from "./Display.module.scss";
 
 export default function Display_item(){
     return(
-        <div>
-            <h1>名前</h1>
-            <h2>タイトル</h2>
-            <p className={Styles.about}>ここからキャラ紹介を始める</p>
-        </div>
+        <>
+            <div>
+                <h1>名前</h1>
+                <h2>タイトル</h2>
+            </div>
+            <div className={Styles.About}>
+                <p className={Styles.aboutitem}>ここからキャラ紹介を始める</p>
+            </div>
+        </>
     )
 }
