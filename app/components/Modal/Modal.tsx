350365f1285cebee7a09aff42764ef567a3115a0
Tomoryo211 <ryo.tomo.211@gmail.com>
2026-01-18 02:54:48 +0900

    Modak表示スタイル変更

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "./Modal.module.scss";
+import VanillaTilt from "vanilla-tilt";
 import { Character } from "../Display/Display";
 
 type Props = {
@@ -10,15 +12,40 @@ type Props = {
 };
 
 export default function Modal({ character, onClose }: Props) {
+    const tiltRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (!tiltRef.current) return;
+
+        VanillaTilt.init(tiltRef.current, {
+            max: 15,
+            speed: 400,
+            glare: true,
+            "max-glare": 0.25,
+        });
+
+    return () => {
+        tiltRef.current?.vanillaTilt?.destroy();
+    };
+}, []);
+
     return (
         <div className={styles.overlay} onClick={onClose}>
-        <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <div
+            className={styles.modal}
+            onClick={(e) => e.stopPropagation()}
+            ref={tiltRef}
+        >
+            <div className={styles.modalImageWrap}>
             <Image
-            src={character.image}
-            alt={character.name}
-            width={300}
-            height={400}
+                src={character.image}
+                alt={character.name}
+                width={300}
+                height={400}
+                className={styles.modalImage}
             />
+            </div>
+
             <h2>{character.name}</h2>
             <p>ここにキャラの詳細説明を書くペン</p>
 
